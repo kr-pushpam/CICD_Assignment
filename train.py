092c97982380a5314607aaa276cb7413dd925b65
kr_pushpam <kr_pushpam@live.com>
2024-04-08 02:55:58 +0530

    changes in code for testcase 2- both actions fail

--- train.py
+++ train.py
@@ -1,7 +1,33 @@
-#Note - this verison of code works 
+#Note - this verison of code works for Case 1- Both of your actions pass
+# import pandas as pd
+# from sklearn.linear_model import LogisticRegression
+# import pickle
+# import numpy as np
+
+# df = pd.read_csv("data/train.csv")
+# X = df.drop(columns=['Disease']).to_numpy()
+# y = df['Disease'].to_numpy()
+# labels = np.sort(np.unique(y))
+# y = np.array([np.where(labels == x) for x in y]).flatten()
+
+# model = LogisticRegression().fit(X, y)
+
+# with open("model.pkl", 'wb') as f:
+#     pickle.dump(model, f)
+
+
+# Case 1- Both of your actions pass - passed 02:45 - tran triggred by pull - and test on completion of train
+
+#################################################################################################################
+
+# Test Case 2 - Both of your actions fail ( both needs to run and fail)
+# Changes -  Train.yml and train.py
+
+
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
-import pickle
+# Intentional error: missing import for pickle
+# import pickle
 import numpy as np
 
 df = pd.read_csv("data/train.csv")
@@ -10,7 +36,10 @@ y = df['Disease'].to_numpy()
 labels = np.sort(np.unique(y))
 y = np.array([np.where(labels == x) for x in y]).flatten()
 
-model = LogisticRegression().fit(X, y)
+# Intentional error: LogisticRegression is incorrectly instantiated
+model = LogisticRegression(wrong_parameter=True).fit(X, y)
 
+# Intentional error: 'pickle' is not imported, so this will fail
 with open("model.pkl", 'wb') as f:
     pickle.dump(model, f)
+
